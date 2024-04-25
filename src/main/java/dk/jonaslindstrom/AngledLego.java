ba757e7dde51ead10f9884c23e2ec60bbb2c06d5
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2024-04-25 10:05:01 +0200

    Adjustable step

--- src/main/java/dk/jonaslindstrom/AngledLego.java
+++ src/main/java/dk/jonaslindstrom/AngledLego.java
@@ -14,27 +14,33 @@ public class AngledLego {
     public static void main(String[] arguments) {
 
         // Default values.
+        double stepX = 0.5;
         double maxX = 15;
+        double stepY = 0.5;
         double maxY = 15;
         double threshold = DEFAULT_THRESHOLD;
 
         // Parse command line arguments.
-        if (arguments.length == 3) {
+        if (arguments.length == 5) {
             maxX = Double.parseDouble(arguments[0]);
-            maxY = Double.parseDouble(arguments[1]);
-            threshold = Double.parseDouble(arguments[2]);
-        } else if (arguments.length == 2) {
+            stepX = Double.parseDouble(arguments[1]);
+            maxY = Double.parseDouble(arguments[2]);
+            stepY = Double.parseDouble(arguments[3]);
+            threshold = Double.parseDouble(arguments[4]);
+        } else if (arguments.length == 4) {
             maxX = Double.parseDouble(arguments[0]);
-            maxY = Double.parseDouble(arguments[1]);
+            stepX = Double.parseDouble(arguments[1]);
+            maxY = Double.parseDouble(arguments[2]);
+            stepY = Double.parseDouble(arguments[3]);
         } else if (arguments.length == 0) {
             // Do nothing.
         } else {
-            System.err.println("Usage: java AngledLego [maxX maxY threshold]");
+            System.err.println("Usage: java AngledLego [maxX stepX maxY stepY [threshold]]");
             System.exit(1);
         }
 
         // Create all possible triples sorted by angle.
-        List<Triple> triples = generateTriples(maxX, maxY, threshold);
+        List<Triple> triples = generateTriples(maxX, stepX, maxY, stepY, threshold);
 
         // Print LaTeX friendly list of triples.
         triples.forEach(System.out::println);
@@ -45,10 +51,10 @@ public class AngledLego {
      * Generate all possible near Pythagorean triples (x, y, z) where x, y are positive integers or half-integers and x <= maxX,
      * y <= maxY and such that the relative distance from z to the nearest integer of half-integer is less than the threshold.
      */
-    private static List<Triple> generateTriples(double maxX, double maxY, double threshold) {
+    private static List<Triple> generateTriples(double maxX, double stepX, double maxY, double stepY, double threshold) {
         // Create all possible triples sorted by angle.
-        return DoubleStream.iterate(0.5, x -> x <= maxX, x -> x + 0.5).boxed()
-                .flatMap(x -> DoubleStream.iterate(0.5, y -> y <= Math.min(maxY, x), y -> y + 0.5)
+        return DoubleStream.iterate(stepX, x -> x <= maxX, x -> x + stepX).boxed()
+                .flatMap(x -> DoubleStream.iterate(stepY, y -> y <= Math.min(maxY, x), y -> y + stepY)
                         .mapToObj(y -> new Triple(x, y)))
                 .filter(triple -> triple.isValid(threshold)).sorted(Comparator.comparingDouble(Triple::getAngle)).toList();
     }
@@ -57,13 +63,6 @@ public class AngledLego {
      * This represents a near Pythagorean triple.
      */
     private record Triple(double x, double y) {
-
-        private static final DecimalFormatSymbols DECIMAL_POINT = DecimalFormatSymbols.getInstance();
-        private static final DecimalFormat FORMAT = new DecimalFormat("#.#", DECIMAL_POINT);
-
-        static {
-            DECIMAL_POINT.setDecimalSeparator('.');
-        }
 
         /**
          * Get the length of the hypotenuse.
@@ -97,14 +96,18 @@ public class AngledLego {
         }
 
         public String toString() {
+            DecimalFormatSymbols decimal_point = DecimalFormatSymbols.getInstance();
+            decimal_point.setDecimalSeparator('.');
+            DecimalFormat format = new DecimalFormat("#.#", decimal_point);
+
             // Output is in LaTeX friendly format.
-            return FORMAT.format(getAngle()) +
+            return format.format(getAngle()) +
                     "°&: (" +
-                    FORMAT.format(x) +
+                    format.format(x) +
                     ", " +
-                    FORMAT.format(y) +
+                    format.format(y) +
                     ", " +
-                    FORMAT.format(getHypotenuse()) +
+                    format.format(getHypotenuse()) +
                     "),\\newline";
         }
     }
